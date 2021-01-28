2be690fd3e2e5336c752647e3953eaae09f6eb6a
Katherine Trammell <ktrammell.com>
2021-01-28 16:28:24 -0500

    Added ability to search by name, city etc

--- Program.cs
+++ Program.cs
@@ -36,6 +36,87 @@ namespace ApiClient
             [JsonPropertyName("updated_at")] public DateTime UpdatedAt { get; set; }
             [JsonPropertyName("created_at")] public DateTime CreatedAt { get; set; }
         }
+
+        static async Task GetBreweryByName(string nameToLookup)
+        {
+            try
+            {
+                var client = new HttpClient();
+
+                var responseFromApi = await client.GetStreamAsync($"https://api.openbrewerydb.org/breweries?by_name={nameToLookup}");
+
+                var breweries = await JsonSerializer.DeserializeAsync<List<Brewery>>(responseFromApi);
+
+                var table = new ConsoleTable("Name", "City", "State", "Brewery Type");
+
+                foreach (var brewery in breweries)
+                {
+                    // Add one row to our table
+                    table.AddRow(brewery.Name, brewery.City, brewery.State, brewery.BreweryType);
+                }
+                table.Write();
+
+
+            }
+            catch (HttpRequestException)
+            {
+                Console.WriteLine("Could not find that Brewery");
+            }
+
+        }
+        static async Task GetBreweryByCity(string cityToLookup)
+        {
+            try
+            {
+                var client = new HttpClient();
+
+                var responseFromApi = await client.GetStreamAsync($"https://api.openbrewerydb.org/breweries?by_city={cityToLookup}");
+
+                var breweries = await JsonSerializer.DeserializeAsync<List<Brewery>>(responseFromApi);
+
+                var table = new ConsoleTable("Name", "City", "State", "Brewery Type");
+
+                foreach (var brewery in breweries)
+                {
+                    // Add one row to our table
+                    table.AddRow(brewery.Name, brewery.City, brewery.State, brewery.BreweryType);
+                }
+                table.Write();
+
+
+            }
+            catch (HttpRequestException)
+            {
+                Console.WriteLine("Could not find a Brewery in that city");
+            }
+
+        }
+        static async Task GetBreweryByPostal(string zipToLookup)
+        {
+            try
+            {
+                var client = new HttpClient();
+
+                var responseFromApi = await client.GetStreamAsync($"https://api.openbrewerydb.org/breweries?by_postal={zipToLookup}");
+
+                var breweries = await JsonSerializer.DeserializeAsync<List<Brewery>>(responseFromApi);
+
+                var table = new ConsoleTable("Name", "City", "State", "Brewery Type");
+
+                foreach (var brewery in breweries)
+                {
+                    // Add one row to our table
+                    table.AddRow(brewery.Name, brewery.City, brewery.State, brewery.BreweryType);
+                }
+                table.Write();
+
+            }
+            catch (HttpRequestException)
+            {
+                Console.WriteLine("Could not find a Brewery in that zip");
+            }
+
+        }
         static async Task Main(string[] args)
         {
             var client = new HttpClient();
@@ -44,9 +125,64 @@ namespace ApiClient
 
             var breweries = await JsonSerializer.DeserializeAsync<List<Brewery>>(responseFromApi);
 
-            foreach (var brewery in breweries)
+            Console.WriteLine("NAME - Search by Name");
+            Console.WriteLine("CITY - Search by City");
+            Console.WriteLine("ZIP - Search by Zip Code");
+            Console.WriteLine("ALL - View ALL Breweries");
+            Console.WriteLine("QUIT - Leave the program");
+            Console.WriteLine();
+
+            Console.Write("Make a selection from our menu options: ");
+            var selection = Console.ReadLine().ToUpper().Trim();
+            Console.WriteLine();
+
+            var userHasChosenToQuit = false;
+            while (userHasChosenToQuit == false)
             {
-                Console.WriteLine($"The brewery {brewery.Name} is located in {brewery.State}");
+                switch (selection)
+                {
+                    case "NAME":
+                        Console.Write("What name would you like to search by? ");
+                        var name = Console.ReadLine();
+                        await GetBreweryByName(name);
+
+                        Console.WriteLine("Press ENTER to continue");
+                        Console.ReadLine();
+
+                        break;
+                    case "ALL":
+                        var table = new ConsoleTable("Name", "City", "State", "BreweryType");
+
+                        foreach (var brewery in breweries)
+                        {
+                            // Add one row to our table
+                            table.AddRow(brewery.Name, brewery.City, brewery.State, brewery.BreweryType);
+                        }
+                        table.Write();
+                        break;
+                    case "CITY":
+                        Console.Write("What city would you like to search by? ");
+                        var city = Console.ReadLine();
+                        await GetBreweryByCity(city);
+
+                        Console.WriteLine("Press ENTER to continue");
+                        Console.ReadLine();
+
+                        break;
+                    case "ZIP":
+                        Console.Write("What city would you like to search by? ");
+                        var zip = Console.ReadLine();
+                        await GetBreweryByCity(zip);
+
+                        Console.WriteLine("Press ENTER to continue");
+                        Console.ReadLine();
+
+                        break;
+                    case "QUIT":
+                        userHasChosenToQuit = true;
+                        Console.WriteLine("Thanks for stopping by!");
+                        break;
+                }
             }
         }
     }
